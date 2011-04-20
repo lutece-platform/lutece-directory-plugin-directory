120cde4c6fc4d430343a956d183ab6f37139dca7
RMA <RMA@bab10101-e421-0410-a517-8ce0973de3ef>
2011-04-20 22:01:40 +0000

    DIRECTORY-105 : Use the ResourceEventManager
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/directory-form-workflow/plugin-directory@28636 bab10101-e421-0410-a517-8ce0973de3ef

---
+++ src/java/fr/paris/lutece/plugins/directory/utils/DirectoryIndexerUtils.java
@@ -0,0 +1,12 @@
+package fr.paris.lutece.plugins.directory.utils;
+
+/**
+ * 
+ * DirectoryIndexerUtils
+ *
+ */
+public class DirectoryIndexerUtils
+{
+	// Indexed resource type name
+	public static final String CONSTANT_TYPE_RESOURCE = "DIRECTORY_DIRECTORY";
+}
