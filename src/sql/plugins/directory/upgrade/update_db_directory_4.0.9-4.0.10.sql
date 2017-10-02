54677969e1c9d2b8fff43f6ace35bb810aad2520
ATOS-NTY <nicolas.tanqueray@atos.net>
2017-10-02 10:35:40 +0200

    DIRECTORY-283 - add SQL update script, fix template

---
+++ src/sql/plugins/directory/upgrade/update_db_directory_4.0.9-4.0.10.sql
@@ -0,0 +1,26 @@
+REPLACE INTO directory_action VALUES (1, 'directory.action.modify.name', 'directory.action.modify.description', 'jsp/admin/plugins/directory/ModifyDirectory.jsp', 'edit', 'MODIFY', 0);
+REPLACE INTO directory_action VALUES (2, 'directory.action.modify.name', 'directory.action.modify.description', 'jsp/admin/plugins/directory/ModifyDirectory.jsp', 'edit', 'MODIFY', 1);
+REPLACE INTO directory_action VALUES (3, 'directory.action.manage_record.name', 'directory.action.manage_record.description', 'jsp/admin/plugins/directory/ManageDirectoryRecord.jsp', 'folder-open', 'MANAGE_RECORD', 0);
+REPLACE INTO directory_action VALUES (4, 'directory.action.manage_record.name', 'directory.action.manage_record.description', 'jsp/admin/plugins/directory/ManageDirectoryRecord.jsp', 'folder-open', 'MANAGE_RECORD', 1);
+REPLACE INTO directory_action VALUES (5, 'directory.action.import_record.name', 'directory.action.import_record.description', 'jsp/admin/plugins/directory/ImportDirectoryRecord.jsp', 'upload', 'IMPORT_RECORD', 0);
+REPLACE INTO directory_action VALUES (6, 'directory.action.import_record.name', 'directory.action.import_record.description', 'jsp/admin/plugins/directory/ImportDirectoryRecord.jsp', 'upload', 'IMPORT_RECORD', 1);
+REPLACE INTO directory_action VALUES (7, 'directory.action.delete_all_directory_record.name', 'directory.action.delete_all_directory_record.description', 'jsp/admin/plugins/directory/ConfirmRemoveAllDirectoryRecord.jsp', 'trash-o', 'DELETE_ALL_RECORD', 0);
+REPLACE INTO directory_action VALUES (8, 'directory.action.delete_all_directory_record.name', 'directory.action.delete_all_directory_record.description', 'jsp/admin/plugins/directory/ConfirmRemoveAllDirectoryRecord.jsp', 'trash-o', 'DELETE_ALL_RECORD', 1);
+REPLACE INTO directory_action VALUES (9, 'directory.action.disable.name', 'directory.action.disable.description', 'jsp/admin/plugins/directory/ConfirmDisableDirectory.jsp', 'remove', 'CHANGE_STATE', 1);
+REPLACE INTO directory_action VALUES (10, 'directory.action.enable.name', 'directory.action.enable.description', 'jsp/admin/plugins/directory/DoEnableDirectory.jsp', 'check', 'CHANGE_STATE', 0);
+REPLACE INTO directory_action VALUES (11, 'directory.action.copy.name', 'directory.action.copy.description', 'jsp/admin/plugins/directory/DoCopyDirectory.jsp', 'clone', 'COPY', 0);
+REPLACE INTO directory_action VALUES (12, 'directory.action.copy.name', 'directory.action.copy.description', 'jsp/admin/plugins/directory/DoCopyDirectory.jsp', 'clone', 'COPY', 1);
+REPLACE INTO directory_action VALUES (13, 'directory.action.delete.name', 'directory.action.delete.description', 'jsp/admin/plugins/directory/ConfirmRemoveDirectory.jsp', 'trash', 'DELETE', 0);
+
+REPLACE INTO directory_record_action VALUES (1, 'directory.action.modify.name', 'directory.action.modify.description.record', 'jsp/admin/plugins/directory/ModifyDirectoryRecord.jsp', 'edit', 'MODIFY_RECORD', 0);
+REPLACE INTO directory_record_action VALUES (2, 'directory.action.modify.name', 'directory.action.modify.description.record', 'jsp/admin/plugins/directory/ModifyDirectoryRecord.jsp', 'edit', 'MODIFY_RECORD', 1);
+REPLACE INTO directory_record_action VALUES (3, 'directory.action.copy.name', 'directory.action.copy.description', 'jsp/admin/plugins/directory/DoCopyDirectoryRecord.jsp', 'clone', 'COPY_RECORD', 0);
+REPLACE INTO directory_record_action VALUES (4, 'directory.action.copy.name', 'directory.action.copy.description', 'jsp/admin/plugins/directory/DoCopyDirectoryRecord.jsp', 'clone', 'COPY_RECORD', 1);
+REPLACE INTO directory_record_action VALUES (5, 'directory.action.delete.name', 'directory.action.delete.description', 'jsp/admin/plugins/directory/ConfirmRemoveDirectoryRecord.jsp', 'trash', 'DELETE_RECORD', 0);
+REPLACE INTO directory_record_action VALUES (6, 'directory.action.disable.name', 'directory.action.disable.description', 'jsp/admin/plugins/directory/ConfirmDisableDirectoryRecord.jsp', 'remove', 'CHANGE_STATE_RECORD', 1);
+REPLACE INTO directory_record_action VALUES (7, 'directory.action.enable.name', 'directory.action.enable.description', 'jsp/admin/plugins/directory/DoEnableDirectoryRecord.jsp', 'check', 'CHANGE_STATE_RECORD', 0);
+REPLACE INTO directory_record_action VALUES (8, 'directory.action.delete.name', 'directory.action.delete.description', 'jsp/admin/plugins/directory/ConfirmRemoveDirectoryRecord.jsp', 'trash', 'DELETE_RECORD', 1);
+REPLACE INTO directory_record_action VALUES (9, 'directory.action.history.name', 'directory.action.history.description', 'jsp/admin/plugins/directory/ResourceHistory.jsp', 'list', 'HISTORY_RECORD', 0);
+REPLACE INTO directory_record_action VALUES (10, 'directory.action.history.name', 'directory.action.history.description', 'jsp/admin/plugins/directory/ResourceHistory.jsp', 'list', 'HISTORY_RECORD', 1);
+REPLACE INTO directory_record_action VALUES (11, 'directory.actions.title.visualisation', 'directory.actions.title.visualisation', 'jsp/admin/plugins/directory/DoVisualisationRecord.jsp', 'eye', 'VISUALISATION_RECORD', 0);
+REPLACE INTO directory_record_action VALUES (12, 'directory.actions.title.visualisation', 'directory.actions.title.visualisation', 'jsp/admin/plugins/directory/DoVisualisationRecord.jsp', 'eye', 'VISUALISATION_RECORD', 1);
